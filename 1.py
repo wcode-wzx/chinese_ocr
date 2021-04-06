dc84a62d3c1e3f1f66c43b3905e325d40c120763
wcode-wzx <17839623189@163.com>
2021-04-06 17:05:50 +0800

    server

--- 1.py
+++
@@ -1,6 +0,0 @@
-import os
-filePath = 'C:\\Users\\thyme\\Desktop\\train\\0'
-for dirpath, dirnames, filenames in os.walk(filePath):
-    path = [os.path.join(dirpath, names) for names in filenames]
-    for fileP in path:
-        print(fileP)
